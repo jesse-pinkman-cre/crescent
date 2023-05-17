befbc7e6e563707dd8e7e8d47f3db708b532af1c
Hyunwoo Lee <124245155+zsystm@users.noreply.github.com>
2023-05-17 13:08:47 +0900

    code-review: clean codes
    updated codes more readable

--- x/referral/keeper/referral.go
+++ x/referral/keeper/referral.go
@@ -9,31 +9,28 @@ import (
 )
 
 func (k Keeper) AddReferral(ctx sdk.Context, fromAddr sdk.AccAddress, code string) (referral types.Referral, err error) {
-	referral, found := k.GetReferralByAddr(ctx, fromAddr)
-
-	// empty liquidity pool
-	if !found {
-		// check whether code already exists
-		_, found := k.GetReferralByCode(ctx, code)
-		if found {
-			err = errors.New("duplicate code name")
-			return
-		}
-
-		referralID := k.GetLastReferralID(ctx)
-		referralID++
-		k.SetLastReferralID(ctx, referralID)
-
-		referral = types.NewReferral(referralID, fromAddr.String(), code)
-		k.SetReferral(ctx, referral)
-		k.SetReferralByAddrIndex(ctx, fromAddr, referralID)
-		k.SetReferralByCodeIndex(ctx, code, referralID)
-	} else {
+	_, found := k.GetReferralByAddr(ctx, fromAddr)]
+	if found {
 		err = errors.New("referral already exists")
 		return
 	}
 
-	return referral, nil
+	_, found = k.GetReferralByCode(ctx, code)
+	if found {
+		err = errors.New("duplicate code name")
+		return
+	}
+
+	referralID := k.GetLastReferralID(ctx)
+	referralID++
+	k.SetLastReferralID(ctx, referralID)
+
+	referral = types.NewReferral(referralID, fromAddr.String(), code)
+	k.SetReferral(ctx, referral)
+	k.SetReferralByAddrIndex(ctx, fromAddr, referralID)
+	k.SetReferralByCodeIndex(ctx, code, referralID)
+
+	return
 }
 
 func (k Keeper) SetParent(ctx sdk.Context, fromAddr sdk.AccAddress, parent string) (referral types.Referral, err error) {
